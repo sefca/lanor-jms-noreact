2f1eb0bec44ba0c21fc4f660df5c42bc93856e2c
Steven Ficko <sficko@rates.ca>
2026-03-27 10:19:38 -0400

    Add cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
@@ -11,8 +13,11 @@ export default defineConfig({
       }
     }
   },
+
   server: {
     port: 3000,
     host: '0.0.0.0'
-  }
+  },
+
+  adapter: cloudflare()
 });
